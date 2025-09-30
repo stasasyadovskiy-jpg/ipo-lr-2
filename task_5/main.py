844ec173be62ed40bf5bcbd4371959fa49992c8a
Станислав <stasasyadovskiy@gmail.com>
2025-10-01 00:21:59 +0300

    Наступны прыпынак

--- task_5/main.py
+++ task_5/main.py
@@ -4,5 +4,5 @@ b = int(input("И вторую тоже: "))
 c = int(input("Введите длину боковой стороны трапеции: "))
 d = int(input("И вторую тоже: "))
 p = (a+b+c+d) / 2
-S = ((a+b) / math.fabs(a-b)) * (math.sqrt((p-a)*(p-b)*(p-a-c)*(p-a-d)))
+S = ((a+b) / math.fabs(a-b)) * (math.sqrt((p-a)*(p-b)*(p-a-c)*(p-a-d)))  
 print(S)
